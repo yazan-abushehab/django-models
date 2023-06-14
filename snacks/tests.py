85616326531ec7344b1b6cab6d9aecf9742c9e01
yazan2181993 <yazanshhab123@gmail.com>
2023-06-14 21:32:18 +0000

    done

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,49 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from .models import Snack
+
 
 # Create your tests here.
+class SnacksTests(TestCase):
+    def setUp(self):
+        purchaser = get_user_model().objects.create(username="tester",password="tester")
+        Snack.objects.create(name="rake", purchaser=purchaser)
+
+    def test_list_page_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_list_page_context(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        things = response.context["snacks"]
+        self.assertEqual(len(things), 1)
+        self.assertEqual(things[0].name, "rake")
+        self.assertEqual(things[0].purchaser.username, "tester")
+
+    def test_detail_page_status_code(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_page_template(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_detail.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_detail_page_context(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        snack = response.context['snack']
+        self.assertEqual(snack.name, "rake")
+        self.assertEqual(snack.rating, 0)
+        self.assertEqual(snack.purchaser.username, "tester")
